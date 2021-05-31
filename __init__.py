3d3cadcada66d6d6b340a288ca29b103c5cb33e3
DaniloToroL <danilo.toro@usach.cl>
2021-05-31 13:13:35 -0400

    [refactor]

--- __init__.py
+++ __init__.py
@@ -50,150 +50,131 @@ def makeTmpDir(name):
 
     return os.sep.join(["tmp", name])
 
+def find_shape(shapes, condition, filter_type="shape_id"):
+    for shape in shapes:
+        print(shape.shape_id, type(shape.shape_id), condition, type(condition))
+        if condition == getattr(shape, filter_type):
+            print(shape)
+            return shape
+
+def change_text_frame(paragraph, text, align="", bold=None, italic=None, underline=None, size=""):
+            paragraph.text = text
+        
+            if size:
+                paragraph.font.size = size
+            if bold is not None:
+                paragraph.font.bold = bold
+            if italic is not None:
+                paragraph.font.italic = italic
+            paragraph.font.underline = underline
+            paragraph.alignment = align
+
+ALIGMENT = {
+            "left": PP_ALIGN.LEFT,
+            "center": PP_ALIGN.CENTER,
+            "right": PP_ALIGN.RIGHT,
+            "justify": PP_ALIGN.JUSTIFY
+        }
 
 module = GetParams("module")
 global prs
 global slide
 global slide_layout
 
-if module == "new":
-    try:
+try:
+    if module == "new":
         prs = Presentation()
-    except Exception as e:
-        print("\x1B[" + "31;40mAn error occurred\x1B[" + "0m")
-        PrintException()
-        raise e
 
-
-if module == "open":
-    try:
+    if module == "open":
+   
         path = GetParams("path")
         prs = None
         prs = Presentation(path)
-    except Exception as e:
-        print("\x1B[" + "31;40mAn error occurred\x1B[" + "0m")
-        PrintException()
-        raise e
 
-elif module == "get_slides_types":
-    res = GetParams("res")
-    list_types = []
-    try:
+    elif module == "get_slides_types":
+        res = GetParams("res")
+        list_types = []
+    
         for type_slide in prs.slide_layouts:
             list_types.append(type_slide.name)
-    except Exception as e:
-        print("\x1B[" + "31;40mAn error occurred\x1B[" + "0m")
-        PrintException()
-        raise e
-    SetVar(res, list_types)
+    
+        SetVar(res, list_types)
 
 
-elif module == "new_slide":
-    name = GetParams("type")    
-    try:
+    elif module == "new_slide":
+        name = GetParams("type")    
+
         list_types = []
         for type_slide in prs.slide_layouts:
+            print(type_slide.name)
             list_types.append(type_slide.name)
         try:
             name = int(name)
             name = list_types[name]
         except ValueError: 
             pass
-        print("name",name)
         slide_layout = prs.slide_layouts.get_by_name(name)
-        print("slide_layout",slide_layout)
         slide = prs.slides.add_slide(slide_layout)
-    except Exception as e:
-        print("\x1B[" + "31;40mAn error occurred\x1B[" + "0m")
-        PrintException()
-        raise e
 
-elif module == "save":
-    path = GetParams("path")
-    try:
-        print(path)
-        print(prs)
+    elif module == "save":
+        path = GetParams("path")
+    
         if path:
             prs.save(path)
-    except Exception as e:
-        print("\x1B[" + "31;40mAn error occurred\x1B[" + "0m")
-        PrintException()
-        raise e
-elif module == "write":
-    text = GetParams("text").replace("\\n", "\n")
-    type_ = GetParams("type")
-    align = GetParams("align")
-    size = GetParams("size")
-    bold = GetParams("bold")
-    ital = GetParams("italic")
-    under = GetParams("underline")
+    
 
-    try:
-        if align == "left":
-            align = PP_ALIGN.LEFT
-        elif align == "center" or None:
-            align = PP_ALIGN.CENTER
-        elif align == "right":
-            align = PP_ALIGN.RIGHT
-        elif align == "justify":
-            align = PP_ALIGN.JUSTIFY
-
-        if size:
-            size = Pt(int(size))
-        if bold:
-            bold = eval(bold)
-        if ital:
-            ital = eval(ital)
-        if under:
-            under = eval(under)
-        slide = prs.slides[0]
-        placeholders = slide.shapes
-        #print(dir(slide.shapes.placeholders.element))
-        # shape = slide.shapes
-        p = placeholders.placeholders.element.add_placeholder(id_ = 2, name = "title", ph_type=1, orient = "horz", sz= "full", idx = 9)
-        from pptx.text.text import TextFrame
-        #p_text_frame = TextFrame(new_ph.txBody, new_ph)
-        #p = _Paragraph(new_ph.txBody.p_lst[0], new_ph)
-        print(dir(p))
-        #p = p_text_frame.paragraphs[0]
-        #print(dir(p.txBody.p_lst))
-        #print(dir(placeholders.placeholders.element))
-        """
-        list_shape = slide.shapes
-        print(len(list_shape))
-        print(dir(list_shape[0].text_frame))
-        print(len(list_shape[0].text_frame.paragraphs))
-        print(dir(list_shape[0].text_frame.paragraphs[0]))
-        list_shape[0].text_frame.paragraphs[0].text = "Buen dia chiques"
-        print(list_shape[0].text_frame.paragraphs[0].text)
-        """
-        #for pl in placeholders.placeholders:
-        #    print(pl.name)
-        #exit()
-        p.text = text
+    elif module == "write":
+        text = GetParams("text").replace("\\n", "\n")
+        slide_index = GetParams("slide_index")
+        shape_index = GetParams("type")
+        align_type = GetParams("align")
+        size = GetParams("size")
+        is_bold = GetParams("bold")
+        is_ital = GetParams("italic")
+        is_under = GetParams("underline")
+        
+        align = ALIGMENT["center"]
+        if align_type is not None:
+            align = ALIGMENT[align_type]
         
         if size:
-            p.font.size = size
-        p.font.bold = bold
-        p.font.italic = ital
-        p.font.underline = under
-        p.alignment = align
-        p.x = 500
-        p.y = 300
-    except Exception as e:
-        PrintException()
-        raise e
+            size = Pt(int(size))
 
-elif module == "close":
-    prs = None
+        bold = ital = under = False
+        if is_bold is not None:
+            bold = eval(is_bold)
+        if is_ital is not None:
+            ital = eval(is_ital)
+        if is_under is not None:
+            under = eval(is_under)
 
-elif module == "add_pic":
+        if not slide_index.isdigit():
+            raise Exception("Slide index is not valid")
 
-    img_path = GetParams("img_path")
-    index = GetParams("slide")
-    pos = GetParams("position")
-    height = GetParams("height")
-    try:
+        slide = prs.slides[int(slide_index)]
+        shapes = slide.shapes
+        for e in shapes:
+            print(e.name)
+        if not shape_index.isdigit():
+            shape = find_shape(shapes, shape_index, "name")
+        else:
+            shape = find_shape(shapes, shape_index)
+       
+        
+        p = shape.text_frame.paragraphs[0]
+        change_text_frame(p, text, align, bold, ital, under, size)
+        
+
+    elif module == "close":
+        prs = None
+
+    elif module == "add_pic":
+
+        img_path = GetParams("img_path")
+        index = GetParams("slide")
+        pos = GetParams("position")
+        height = GetParams("height")
+
         index = int(index)
         if index:
             slide = prs.slides[index]
@@ -204,7 +185,6 @@ elif module == "add_pic":
             list_types.append(type_slide.name)
         if slide_layout.name == list_types[8]:
             for placeholder in placeholders:
-                print(placeholder.name.lower())
                 if "picture" in placeholder.name.lower():
                     placeholder.insert_picture(img_path)
         else:
@@ -216,16 +196,35 @@ elif module == "add_pic":
                     img_path, left=left, top=top, height=height)
             else:
                 pic = slide.shapes.add_picture(img_path, left=left, top=top)
-    except Exception as e:
-        PrintException()
-        raise e
 
-elif module == "addTextbox":
-    text = GetParams("text")
-    position = GetParams("position")
-    size = GetParams("size")
+    elif module == "addTextbox":
+        text = GetParams("text")
+        position = GetParams("position")
+        size = GetParams("size")
+        index = GetParams("slide")
+        align_type = GetParams("align")
+       
+        is_bold = GetParams("bold")
+        is_ital = GetParams("italic")
+        is_under = GetParams("underline")
+        
+        align = ALIGMENT["center"]
+        if align_type:
+            align = ALIGMENT[align_type]
+        
 
-    try:
+        bold = ital = under = False
+        if is_bold is not None:
+            bold = eval(is_bold)
+        if is_ital is not None:
+            ital = eval(is_ital)
+        if is_under is not None:
+            under = eval(is_under)
+
+        # if not index.isdigit():
+        #     raise Exception("Slide index is not valid")
+
+        # slide = prs.slides[int(index)]
         position = position.split(",")
         size = size.split(",")
         pos = position + size
@@ -233,43 +232,113 @@ elif module == "addTextbox":
             pos[i] = Pt(int(pos[i]))
 
         txt_box = slide.shapes.add_textbox(pos[0], pos[1], pos[2], pos[3])
+
+        p = txt_box.text_frame.paragraphs[0]
+        change_text_frame(p, text, align, bold, ital, under)
         txt_box.text_frame.text = text
 
-    except Exception as e:
-        PrintException()
-        raise e
 
-if module == "editText":
-    try:
+    elif module == "editText":
+
         search_by = GetParams("searchBy")
         index = GetParams("slide")
         shape_index = GetParams("shape")
         text = GetParams("text")
+        align_type = GetParams("align")
+        size = GetParams("size")
+        is_bold = GetParams("bold")
+        is_ital = GetParams("italic")
+        is_under = GetParams("underline")
 
-        if search_by == "id":
-            slide = prs.slides.get(index)
-        if search_by == "position":
-            slide = prs.slides[int(index)]
-        shape_index = int(shape_index)
-        slide.shapes[shape_index].text_frame.text = text
+        align = ALIGMENT["center"]
+        if align_type:
+            align = ALIGMENT[align_type]
+        
+        if size:
+            size = Pt(int(size))
 
-    except Exception as e:
-        PrintException()
-        raise e
+        bold = ital = under = None
+        if is_bold:
+            bold = eval(is_bold)
+        if is_ital:
+            ital = eval(is_ital)
+        if is_under:
+            under = eval(is_under)
+
+        if not index.isdigit():
+            raise Exception("Slide index is not valid")
+
+        slide = prs.slides[int(index)]
+        shapes = slide.shapes
+        if not shape_index.isdigit():
+            shape = find_shape(shapes, shape_index, "name")
+        else:
+            shape = find_shape(shapes, int(shape_index))
+
+        p = shape.text_frame.paragraphs[0]
+        change_text_frame(p, text, align, bold, ital, under, size)
 
 
-if module == "listSlides":
-    res = GetParams("res")
-    slide_index = int(GetParams("slide_index"))
-    try:
+    elif module == "listSlides":
+        res = GetParams("res")
+        slide_index = int(GetParams("slide_index"))
+    
         text_runs = []
         slide = prs.slides[slide_index]
-        for index, shape in enumerate(slide.shapes):
+        
+        for shape in slide.shapes:
+            
+            text = ""
+            index = shape.shape_id
+            name = shape.name
+            type_ = shape.shape_type
             if shape.has_text_frame:
-                value = (index, shape.text)
-                text_runs.append(value)
+                text = shape.text
+                
+            text_runs.append({
+                "id": index,
+                "name": name,
+                "type": type_,
+                "value": text
+            })
         SetVar(res, text_runs)
-    except Exception as e:
-        print("\x1B[" + "31;40mError\x1B[" + "0m")
-        PrintException()
-        raise e
+
+    elif module == "editElement":
+        index = GetParams("slide_index")
+        shape_index = GetParams("type")
+        pos = GetParams("position")
+        size = GetParams("size")
+        rotation = GetParams("rotation")
+
+
+        if not index.isdigit():
+            raise Exception("Slide index is not valid")
+
+        slide = prs.slides[int(index)]
+        shapes = slide.shapes
+        if not shape_index.isdigit():
+            shape = find_shape(shapes, shape_index, "name")
+        else:
+            shape = find_shape(shapes, int(shape_index))
+
+        if pos:
+            left, top = pos.split(",")
+            shape.left = int(left)
+            shape.top = int(top)
+        if size:
+            width, height = size.split(",")
+            shape.width = int(width)
+            shape.height = int(height)
+
+        if rotation:
+            shape.rotation = float(rotation)
+        
+
+        print(dir(shape))
+
+
+
+except Exception as e:
+    print("\x1B[" + "31;40mError\x1B[" + "0m")
+    PrintException()
+    raise e
